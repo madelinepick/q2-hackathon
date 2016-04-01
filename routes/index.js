a55e561a36f7732abe2ee777f8b842c47f0930e0
Madeline Pickering <madelinepickering@gmail.com>
2016-04-01 11:58:48 -0600

    basic login UI

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/app', function(req, res, next) {
+  res.render('app');
+});
+
+router.post('/signup', function(req, res, next) {
+  res.redirect('app');
+});
+
+router.post('/login', function(req, res, next) {
+  res.redirect('app');
+});
+
 
 module.exports = router;
